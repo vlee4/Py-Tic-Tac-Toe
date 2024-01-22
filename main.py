2bd9b4e70d05910435669e0508aa053fb0e8a384
vlee4 <60161691+vlee4@users.noreply.github.com>
2024-01-22 08:27:41 +0000

    Setting up tic-tac-toe function templates

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 print("Hello World")
+
+
+def display_board(board):
+  # The function accepts one parameter containing the board's current status
+  # and prints it out to the console.
+  return
+
+
+def enter_move(board):
+  # The function accepts the board's current status, asks the user about their move,
+  # checks the input, and updates the board according to the user's decision.
+  return
+
+
+def make_list_of_free_fields(board):
+  # The function browses the board and builds a list of all the free squares;
+  # the list consists of tuples, while each tuple is a pair of row and column numbers.
+  return
+
+
+def victory_for(board, sign):
+  # The function analyzes the board's status in order to check if
+  # the player using 'O's or 'X's has won the game
+  return
+
+
+def draw_move(board):
+  # The function draws the computer's move and updates the board.
+  return
